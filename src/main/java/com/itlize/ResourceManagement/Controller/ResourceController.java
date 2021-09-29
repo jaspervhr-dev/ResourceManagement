74e1a1042b6c5822161ce8d37a1c5c4812d1954c
Cretis <stuartsf1996@gmail.com>
2021-09-29 15:17:16 -0400

    services with resources

---
+++ src/main/java/com/itlize/ResourceManagement/Controller/ResourceController.java
@@ -0,0 +1,33 @@
+package com.itlize.ResourceManagement.Controller;
+
+import com.itlize.ResourceManagement.Entity.Resource;
+import com.itlize.ResourceManagement.Service.ResourceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author Siteng Fan
+ * @date 9/29/21 12:01 PM
+ */
+
+@RestController
+@RequestMapping("resources")
+public class ResourceController {
+
+    @Autowired
+    ResourceService service;
+
+    @GetMapping("/all")
+    public List<Resource> findAll(){
+        return service.findALl();
+    }
+
+    @GetMapping("/{id}")
+    public Resource findById(Integer id){
+        return service.findOneById(id);
+    }
+}
