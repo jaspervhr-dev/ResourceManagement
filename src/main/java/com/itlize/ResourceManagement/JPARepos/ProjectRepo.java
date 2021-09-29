26395313245d5ce57c0901607e10f9f6b34f772c
Jasper <jaspervhr@gmail.com>
2021-09-29 10:20:12 -0400

    Created jpa repo interfaces

---
+++ src/main/java/com/itlize/ResourceManagement/JPARepos/ProjectRepo.java
@@ -0,0 +1,10 @@
+package com.itlize.ResourceManagement.JPARepos;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
+
+import java.io.Serializable;
+
+public interface ProjectRepo<T, ID extends Serializable>
+        extends PagingAndSortingRepository<T, ID>, QueryByExampleExecutor<T> {
+}
